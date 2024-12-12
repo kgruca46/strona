9217073ca506daab3b76610a8a2a82a1d86ccbf1
kgruca46 <kgruca46@uczen.tl.krakow.pl>
2024-12-12 11:43:13 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Moja Strona</title>
+</head>
+<body>
+    <h1>Ksawery Gruca</h1>
+    <img src="https://www.bing.com/images/search?view=detailV2&ccid=U59WpJqu&id=C9D23E5DCEC20AA7AB5D69C6E65052C4310E89E2&thid=OIP.U59WpJquXleUCwLxME5BmwHaE7&mediaurl=https%3a%2f%2ftl.krakow.pl%2ffiles%2fimages%2fbudynek-zsl.l%2c800%2c600.459.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.539f56a49aae5e57940b02f1304e419b%3frik%3d4okOMcRSUObGaQ%26pid%3dImgRaw%26r%3d0&exph=533&expw=800&q=zsl+krakow&simid=608000635115948797&FORM=IRPRST&ck=B9D5A44872950F1765E003BF3175717D&selectedIndex=1&itb=0" alt="Zdjęcie 1" width="300">
+    <img src="https://www.bing.com/images/search?view=detailV2&ccid=lfwqb17Y&id=C9D23E5DCEC20AA7AB5D6B849F48B24CD8C0EF30&thid=OIP.lfwqb17YmjuBvwhMuJUIvAHaFj&mediaurl=https%3a%2f%2ftl.krakow.pl%2ffiles%2fimages%2fbudynek-zsl.l%2c800%2c600.463.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.95fc2a6f5ed89a3b81bf084cb89508bc%3frik%3dMO%252fA2EyySJ%252bEaw%26pid%3dImgRaw%26r%3d0&exph=600&expw=800&q=zsl+krakow&simid=608010479240560861&FORM=IRPRST&ck=F864D9312BED25FB528CBB6F148181D3&selectedIndex=8&itb=0" alt="Zdjęcie 2" width="300">
+</body>
+</html>
+?>
